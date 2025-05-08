925c63cecb858b6c31c3352a251f58150a466d08
Carlota Maroto <cmg0058@alu.medac.es>
2025-05-08 20:39:37 +0200

    Merge de listaNombres y enemigo desde main a carlota

---
+++ enemigo.py
@@ -0,0 +1,10 @@
+class Enemigo:
+    def __init__(self, nombre, lista_digipymons = [], cantidad_digipymon = 0):
+        self.nombre = nombre
+        self.lista_digipymons = lista_digipymons
+        self.cantidad_digipymon = cantidad_digipymon
+
+
+    def añadir_digipymon(self, digipymon):
+        self.lista_digipymons.append(digipymon)
+        self.cantidad_digipymon += 1 
