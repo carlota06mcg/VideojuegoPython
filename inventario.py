37861492c497eb682d56baf844eae8e6d9437263
Carlota Maroto <cmg0058@alu.medac.es>
2025-05-08 21:03:37 +0200

    arreglo creo

---
+++ inventario.py
@@ -0,0 +1,11 @@
+from inventario import jugador
+class inventario:
+    def __init__(self):
+        self.objetos = []
+
+    def añadir_objeto(self, nombre, cantidad)
+        if nombre in self.objetos:
+            self.objetos = self.cantidad + 1
+        else:
+            self.objetos = self.nombre
+            
---
+++ jugador.py
@@ -0,0 +1,17 @@
+class Jugador:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.lista_digipymon = []
+        self.cantidad_digipymon = 0
+        self.digicoins = 10
+
+    def añadir_digipymon(self, nombre):
+        self.lista_digipymon.append(nombre)
+        self.cantidad_digipymon += 1
+        return nombre
+
+    def consultar_digipymon(self, nombre):
+       print("Tu digipymon se llama " + self.lista_digipymon)
+
+    def mostrar_digicoins(self):
+        print("Tienes " + str(self.digicoins) + " digicoins")
