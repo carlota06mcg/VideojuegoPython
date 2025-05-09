30efcdb4a92a55477174647e1b7527b568901669
davidjc05 <jimenezcuellardavid0@gmail.com>
2025-05-09 17:55:07 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def menu():
+    print("Introduce 1 si quieres buscar un digipymon")
+    print("Introduce 2 si quieres luchar contra un entrenador")
+    print("Introduce 3 si quieres ir a la tienda")
+    print("Introduce 4 si quieres usar objetos")
+    print("Introduce 5 si quieres consultar el inventario")
+    print("Introduce 4 si quieres consultar digipymon")
+    print("Introduce 5 si quieres salir")
