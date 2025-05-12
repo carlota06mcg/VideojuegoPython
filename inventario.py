d00c217bcc19558d0e90ad229a02e2956457952d
davidjc05 <jimenezcuellardavid0@gmail.com>
2025-05-12 16:26:00 +0200

    Update inventario.py
    Clase terminada

--- inventario.py
+++ inventario.py
@@ -5,9 +5,10 @@ class Inventario:
 
     def añadir_objeto(self, nombre, cantidad):
         if nombre in self.objetos:
-            self.objetos = [self.nombre] + cantidad
+            self.objetos[self.nombre] + cantidad
+
         else:
-            self.objetos = [self.nombre]
+            self.objetos[self.nombre]
     
     def usar_objeto(self, nombre):
         if nombre in self.objetos:
