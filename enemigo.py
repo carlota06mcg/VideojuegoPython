a2f1813288d1ad94d8f609f7133c0b3f71131934
carlota06mcg <cmg0058@alu.medac.es>
2025-05-18 21:50:18 +0200

    documentacion clase enemigo

--- enemigo.py
+++ enemigo.py
@@ -1,10 +1,20 @@
+"""
+    Clase "Enemigo".
+"""
 class Enemigo:
+    """
+    Función "__init__".
+    Definimos los atributos de nombre, lista_digipymons = [], cantidad_digipymon = 0.
+    """
     def __init__(self, nombre, lista_digipymons = [], cantidad_digipymon = 0):
         self.nombre = nombre
         self.lista_digipymons = lista_digipymons
         self.cantidad_digipymon = cantidad_digipymon
 
-
+    """
+    Función "añadir_digipymon".
+    "lista_digipymon.append(digipymon)" Utilizamos append para añadir el digipymon a la lista.
+    """
     def añadir_digipymon(self, digipymon):
         self.lista_digipymons.append(digipymon)
         self.cantidad_digipymon = self.cantidad_digipymon + 1 
